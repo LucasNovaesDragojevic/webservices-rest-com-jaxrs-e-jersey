1115f69169a19cd8e03bae3effa877e73b37dcc0
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-03-15 21:00:04 -0300

    Criado pequeno servidor

---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,38 @@
+package br.com.alura.loja.modelo;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto {
+	
+	private Long id;
+	private String nome;
+	private Integer anoInicio;
+		
+	public Projeto() {}
+
+	public Projeto(Long id, String nome, Integer anoInicio) {
+		this.id = id;
+		this.nome = nome;
+		this.anoInicio = anoInicio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Integer getAnoInicio() {
+		return anoInicio;
+	}
+	
+	public String toXML() {
+		return new XStream().toXML(this);
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
