f8ffc07e9e9005d518101ebfb3b3fc4a2f064aed
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-03-15 21:22:44 -0300

    Implementadas variáveis de caminho de URL

--- src/test/java/br/com/alura/loja/ProjetoTest.java
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -4,16 +4,31 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProjetoTest {
+
+	private HttpServer server;
+	
+	@Before
+	public void before() {
+		server = Servidor.iniciaServidor();
+	}
+	
+	@After
+	public void after() {
+		server.stop();
+	}
 
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/projetos").request().get(String.class);
+		String conteudo = target.path("/projetos/1").request().get(String.class);
 		System.out.println(conteudo);
 		Assert.assertTrue(conteudo.contains("Itália"));
 	}
